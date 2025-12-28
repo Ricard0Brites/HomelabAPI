aa0f6db63c74da2751556384812448160c6cfc91
Ricardo Brites <ricardo.brites.work@gmail.com>
2025-12-28 07:45:39 +0000

    Added Pseudocode

--- HomelabAPI/app.js
+++ HomelabAPI/app.js
@@ -1,1 +1,8 @@
-
+// If There are updates available
+    // Stop All Services
+    // Fetch Updates
+    // Delete All imports 
+    // for each module
+        // Build
+        // Reimport service
+        // Start service
